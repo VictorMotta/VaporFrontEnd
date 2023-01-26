4ecf0a2d91008c985af15b373e573f78278f2095
Marcio-VOT <marciofsantos65@gmail.com>
2023-01-25 21:28:24 -0300

    feat: Top inico

--- src/components/TopMenu/TopMenu.jsx
+++ src/components/TopMenu/TopMenu.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../context/authContext";
+import { FaUserCircle } from "react-icons/fa";
+import { TopUserImage, TopUserContainer } from "./TopMenuStyled";
 
 const TopMenu = () => {
-  return <div>a</div>;
+  const { setUser, user, authenticated } = useContext(AuthContext);
+
+  // useEffect(() => {
+  //   setUser({
+  //     avatar:
+  //       "https://images.vexels.com/media/users/3/160888/isolated/preview/bc02981b8349e795c78a3dadfdb1770b-ilustracao-de-copo-de-leite.png",
+  //   });
+  // }, []);
+
+  const [search, setSearch] = useState("");
+  return (
+    <TopUserContainer>
+      <input type="text" />
+      <TopUserImage>
+        {authenticated ? (
+          <img src={user.avatar} alt="user" />
+        ) : (
+          <FaUserCircle />
+        )}
+      </TopUserImage>
+    </TopUserContainer>
+  );
 };
 
 export default TopMenu;

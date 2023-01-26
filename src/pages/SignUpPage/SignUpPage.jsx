32346219e047a0919bbb0dc2625fed6c4b6658e2
Leonan <leonanm04@gmail.com>
2023-01-25 21:26:23 -0300

    feat: criando tela de cadastro

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,7 +1,18 @@
+import { Input, Logo, SingUpDiv } from "./SignUpPageStyled";
+import logo from "../../assets/images/Logo.png";
+import { Form } from "react-router-dom";
+
 export default function SignUpPage() {
   return (
-    <div>
-      <h1>SignUpPage</h1>
-    </div>
+    <SingUpDiv>
+      <Logo src={logo} alt="logo" />
+      {/* <Form>
+        <Input type="text" placeholder="Nome" />
+        <Input type="text" placeholder="E-mail" />
+        <Input type="password" placeholder="Senha" />
+        <Input type="password" placeholder="Confirme a senha" />
+        <Input type="submit" value="Cadastrar" />
+      </Form> */}
+    </SingUpDiv>
   );
 }

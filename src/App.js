889c89bd805b3fa7d0c95e3d70703596c8131d23
Victor Motta <victor.mottag@gmail.com>
2023-01-27 19:03:57 -0300

    Fix: Resolve retirar o private do app e colocar em um componente, adicionado também private admin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { AuthContext, AuthProvider } from "./context/authContext";
-import { useContext } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/authContext";
 import HomePage from "./pages/HomePage/HomePage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
 import SignInPage from "./pages/SignInPage/SignInPage";
@@ -8,22 +7,9 @@ import AddProductPage from "./pages/AddProductPage/AddProductPage";
 import MenuPage from "./pages/MenuPage/MenuPage";
 import CartPage from "./pages/CartPage/CartPage";
 import { CartProvider } from "./context/cartContext";
+import { Private, PrivateAdmin } from "./components/Authenticated/Authenticated";
 
 function App() {
-  function Private({ children }) {
-    const { authenticated, loading } = useContext(AuthContext);
-
-    if (loading) {
-      return <div>Carregando...</div>;
-    }
-
-    if (!authenticated) {
-      return <Navigate to='/' />;
-    }
-
-    return children;
-  }
-
   return (
     <BrowserRouter>
       <AuthProvider>
@@ -34,7 +20,16 @@ function App() {
             <Route path='/menu' element={<MenuPage />} />
             <Route path='/cadastrar' element={<SignUpPage />} />
             <Route path='/login' element={<SignInPage />} />
-            <Route path='/cadastro-produto' element={<AddProductPage />} />
+            <Route
+              path='/cadastro-produto'
+              element={
+                <Private>
+                  <PrivateAdmin>
+                    <AddProductPage />
+                  </PrivateAdmin>
+                </Private>
+              }
+            />
             <Route
               path='/pagamento'
               element={

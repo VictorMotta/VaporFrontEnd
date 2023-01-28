6e46939b5b6671ce7d763a7c05581fa30d7c3b97
Marcio-VOT <marciofsantos65@gmail.com>
2023-01-28 02:00:57 -0300

    feat: rota de produto parcial

---
+++ src/components/ProductPriceTag/ProductPriceTagStyled.js
@@ -0,0 +1,93 @@
+import styled from "styled-components";
+
+const ProductPriceTagStyled = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 193px;
+  justify-content: end;
+  position: relative;
+  border-radius: 2px;
+  border: solid 2px black;
+  button {
+    width: 70px;
+    height: 30px;
+    display: flex;
+    align-items: center;
+    font-family: "Roboto Condensed";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 14px;
+    background: linear-gradient(270deg, #588a1b 0%, #75b022 100%);
+    border-style: none;
+    border-left: solid 2px black;
+    color: #ffffff;
+  }
+  h1 {
+    height: 30px;
+    width: 52px;
+    display: flex;
+    padding-left: 3px;
+    padding-right: 2px;
+    background-color: green;
+    justify-content: center;
+    align-items: center;
+    font-family: "Roboto Condensed";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 23px;
+
+    color: #beec19;
+    background: #4b6b22;
+  }
+  div {
+    display: column;
+    height: 30px;
+    background: #1d2e3e;
+  }
+  h2 {
+    height: 15px;
+    width: 71px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: "Roboto Condensed";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 14px;
+    color: #738795;
+  }
+  h3 {
+    height: 15px;
+    width: 71px;
+    color: #b3dc39;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: "Roboto Condensed";
+    font-family: "Roboto Condensed";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 13px;
+    line-height: 15px;
+  }
+  p::before {
+    content: "";
+    position: absolute;
+    width: 25%;
+    border-bottom: 1px solid;
+    transform: rotate(-10deg);
+    bottom: 23px;
+    right: 79px;
+    font-family: "Roboto Condensed";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 11px;
+    line-height: 13px;
+    color: #949599;
+  }
+`;
+export default ProductPriceTagStyled;
--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,14 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import ProductPriceTag from "../../components/ProductPriceTag/ProductPriceTag";
+import {
+  ContainerDisc,
+  ContainerProduct,
+  ContainerBuy,
+} from "./ProductPageStyled";
+import TopMenu from "../../components/TopMenu/TopMenu";
+import { AiOutlineLeft } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+import FooterMenu from "../../components/FooterMenu/FooterMenu";
 
 const ProductPage = () => {
-  const rota = useParams().id;
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [product, setProduct] = useState({});
   useEffect(() => {
-    console.log(rota);
+    axios.get(`http://localhost:5000/products/${id}`).then(({ data }) => {
+      setProduct(data);
+      console.log(data);
+    });
   }, []);
   return (
     <>
-      <div>{"aaaaaaaaaaaaa"}</div>
+      <TopMenu>
+        <div>
+          <AiOutlineLeft onClick={() => navigate(-1)} />
+        </div>
+        <h1>{product.title}</h1>
+      </TopMenu>
+
+      <ContainerProduct>
+        <img src={product.images} alt={product.title} />
+        <ContainerBuy>
+          Comprar {product.title}
+          <ProductPriceTag />
+        </ContainerBuy>
+        <ContainerDisc>
+          <h1>SOBRE ESSE JOGO</h1>
+          <h2>{product.description}</h2>
+        </ContainerDisc>
+      </ContainerProduct>
+      <FooterMenu />
     </>
   );
 };

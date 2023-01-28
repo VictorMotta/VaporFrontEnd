f9acb4b90c24a7a15f797906e24681c36b9ffc5e
Marcio-VOT <marciofsantos65@gmail.com>
2023-01-27 23:10:36 -0300

    feat: rota "/" implementada 100%

---
+++ src/components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { GameContainer } from "./ProductDisplayStyled";
+import { Link } from "react-router-dom";
+
+const ProducDisplay = ({
+  title,
+  category,
+  price,
+  pricePromotion,
+  images,
+  promoPercentage,
+  _id,
+}) => {
+  return (
+    <GameContainer>
+      <Link to={`/produto/${_id}`}>
+        <img src={images} alt={title} />
+        <div>
+          <h1>{title}</h1>
+          <h3>{category}</h3>
+        </div>
+        <PriceTag
+          price={price}
+          pricePromotion={pricePromotion}
+          promoPercentage={promoPercentage}
+        />
+      </Link>
+    </GameContainer>
+  );
+};
+
+const PriceTag = ({ price, pricePromotion, promoPercentage }) => {
+  promoPercentage = Number(promoPercentage);
+  promoPercentage = 1;
+  pricePromotion = "90.99";
+  price = "99.99";
+  if (price) {
+    price = price.replace(".", ",");
+    pricePromotion = pricePromotion.replace(".", ",");
+  }
+
+  return (
+    <>
+      {promoPercentage === 0 ? (
+        <span>
+          <h3>R$ {price}</h3>
+        </span>
+      ) : (
+        <span>
+          <h1> </h1>
+          <h2>R$ {price}</h2>
+          <h3>R$ {pricePromotion}</h3>
+        </span>
+      )}
+    </>
+  );
+};
+
+export default ProducDisplay;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,20 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Image, ImageWrapper, InputSearchHomePage } from "./HomePageStyled";
+import {
+  HomePageContainer,
+  Image,
+  CarrouselWrapper,
+  InputSearchHomePage,
+  ImageWrapper,
+} from "./HomePageStyled";
 import TopMenu from "../../components/TopMenu/TopMenu";
 import FooterMenu from "../../components/FooterMenu/FooterMenu";
 import { AuthContext } from "../../context/authContext";
-
+import axios from "axios";
+import ProducDisplay from "../../components/ProductDisplay/ProductDisplay";
+import BannerPriceTag from "../../components/BannerPriceTag/BannerPriceTag";
 const images = [
   {
     src: "https://thumbs.dreamstime.com/b/paisagens-bonitas-do-mar-43239195.jpg",
@@ -29,6 +37,9 @@ const images = [
 const HomePage = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [search, setSearch] = useState("");
+  const [products, setProducts] = useState([]);
+  const [promoItens, setPromoItens] = useState([]);
+  let currentOffset = 0;
 
   const settings = {
     arrows: false,
@@ -42,13 +53,42 @@ const HomePage = () => {
     adaptiveHeight: true,
     afterChange: (current) => setCurrentSlide(current),
   };
+  const handleScroll = (e) => {
+    const scrollHeight = e.target.documentElement.scrollHeight;
+    const currentHeight = Math.ceil(
+      e.target.documentElement.scrollTop + window.innerHeight
+    );
+    if (currentHeight + 1 >= scrollHeight) {
+      loadFiveProducts();
+    }
+  };
+
+  useEffect(() => {
+    loadFiveProducts();
+    window.addEventListener("scroll", handleScroll);
+    axios
+      .get(`http://localhost:5000/products?offset=0&limit=0`, { promo: "true" })
+      .then(({ data }) => {
+        setPromoItens([...data].sort(() => Math.random() - 0.5).splice(0, 4));
+      });
+  }, []);
+
+  const loadFiveProducts = () => {
+    axios
+      .get(`http://localhost:5000/products?offset=${currentOffset}&limit=5`)
+      .then(({ data }) => {
+        const tenProducts = data;
+        setProducts((products) => [...products, ...tenProducts]);
+      });
+    currentOffset += 5;
+  };
 
   return (
     <>
       <TopMenu>
         <InputSearchHomePage
           search={search}
-          type='text'
+          type="text"
           placeholder={"a"}
           value={search}
           onChange={(e) => {
@@ -56,16 +96,55 @@ const HomePage = () => {
           }}
         />
       </TopMenu>
-      <ImageWrapper>
-        <Slider {...settings}>
-          {images.map((image, index) => (
-            <div key={index}>
-              <Image className='slider-image' src={image.src} alt={image.alt} />
-            </div>
-          ))}
-        </Slider>
-      </ImageWrapper>
-      <FooterMenu />
+      <HomePageContainer>
+        <CarrouselWrapper>
+          <Slider {...settings}>
+            {promoItens.map((game, index) => (
+              <ImageWrapper key={index} to={`/produto/${game._id}`}>
+                <Image
+                  className="slider-image"
+                  src={game.images[0]}
+                  alt={game.title}
+                />
+                <div>{game.title}</div>
+                <h2>{game.description}</h2>
+                <BannerPriceTag
+                  pricePromotion={game.pricePromotion}
+                  price={game.price}
+                  promoPercentage={game.promoPercentage}
+                />
+              </ImageWrapper>
+            ))}
+          </Slider>
+        </CarrouselWrapper>
+        <h1>FEITO PARA VOCÊ</h1>
+        {products.map(
+          (
+            {
+              title,
+              category,
+              price,
+              images,
+              promoPercentage,
+              pricePromotion,
+              _id,
+            },
+            index
+          ) => (
+            <ProducDisplay
+              title={title}
+              category={category}
+              price={price}
+              images={images}
+              promoPercentage={promoPercentage}
+              pricePromotion={pricePromotion}
+              key={index}
+              _id={_id}
+            />
+          )
+        )}
+        <FooterMenu />
+      </HomePageContainer>
     </>
   );
 };
